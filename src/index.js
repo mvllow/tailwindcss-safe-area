3848d936af579773cbf9640b86e470a4372069a9
mvllow <mvllow@icloud.com>
2021-02-15 17:10:33 -0600

    init

--- src/index.js
+++ src/index.js
@@ -1,45 +1,62 @@
 const plugin = require('tailwindcss/plugin')
 
-const lineClamp = plugin(
-  function ({ addUtilities, theme, variants, e }) {
-    const values = theme('lineClamp')
-
-    addUtilities(
-      [
-        Object.entries(values).map(([key, value]) => {
-          return {
-            [`.${e(`line-clamp-${key}`)}`]: {
-              overflow: 'hidden',
-              display: '-webkit-box',
-              '-webkit-box-orient': 'vertical',
-              '-webkit-line-clamp': `${value}`,
-            },
-          }
-        }),
-        {
-          '.line-clamp-none': {
-            '-webkit-line-clamp': 'unset',
-          },
-        },
-      ],
-      variants('lineClamp')
-    )
-  },
-  {
-    theme: {
-      lineClamp: {
-        1: '1',
-        2: '2',
-        3: '3',
-        4: '4',
-        5: '5',
-        6: '6',
-      },
+const safeArea = plugin(({ addUtilities }) => {
+  const newUtilities = {
+    '.m-safe': {
+      marginTop: 'env(safe-area-inset-top)',
+      marginRight: 'env(safe-area-inset-right)',
+      marginBottom: 'env(safe-area-inset-bottom)',
+      marginLeft: 'env(safe-area-inset-left)',
     },
-    variants: {
-      lineClamp: ['responsive'],
+    '.mx-safe': {
+      marginRight: 'env(safe-area-inset-right)',
+      marginLeft: 'env(safe-area-inset-left)',
+    },
+    '.my-safe': {
+      marginTop: 'env(safe-area-inset-top)',
+      marginBottom: 'env(safe-area-inset-bottom)',
+    },
+    '.mt-safe': {
+      marginTop: 'env(safe-area-inset-top)',
+    },
+    '.mr-safe': {
+      marginRight: 'env(safe-area-inset-right)',
+    },
+    '.mb-safe': {
+      marginBottom: 'env(safe-area-inset-bottom)',
+    },
+    '.ml-safe': {
+      marginLeft: 'env(safe-area-inset-left)',
+    },
+    '.p-safe': {
+      paddingTop: 'env(safe-area-inset-top)',
+      paddingRight: 'env(safe-area-inset-right)',
+      paddingBottom: 'env(safe-area-inset-bottom)',
+      paddingLeft: 'env(safe-area-inset-left)',
+    },
+    '.px-safe': {
+      paddingRight: 'env(safe-area-inset-right)',
+      paddingLeft: 'env(safe-area-inset-left)',
+    },
+    '.py-safe': {
+      paddingTop: 'env(safe-area-inset-top)',
+      paddingBottom: 'env(safe-area-inset-bottom)',
+    },
+    '.pt-safe': {
+      paddingTop: 'env(safe-area-inset-top)',
+    },
+    '.pr-safe': {
+      paddingRight: 'env(safe-area-inset-right)',
+    },
+    '.pb-safe': {
+      paddingBottom: 'env(safe-area-inset-bottom)',
+    },
+    '.pl-safe': {
+      paddingLeft: 'env(safe-area-inset-left)',
     },
   }
-)
 
-module.exports = lineClamp
+  addUtilities(newUtilities)
+})
+
+module.exports = safeArea
